b1ef4cf2a4ea20987f7fa66da75c8d38af46d34e
unknown <fnumilat@gmail.com>
2020-05-23 20:35:30 -0500

    all endpoints for recipe are created

---
+++ recipes-router/recipesDataModel.js
@@ -0,0 +1,37 @@
+const db = require("../database/config")
+
+module.exports = {
+    getRecipes,
+    getRecipeById,
+    addRecipe,
+    updateRecipe,
+    deleteRecipe
+}
+
+function getRecipes() {
+    return db("recipes").select("*")
+}
+
+function getRecipeById(id) {
+    return db("recipes")
+    .where({ id })
+    .first()
+}
+
+async function addRecipe(user) {
+    const [ id ] = await db("recipes").insert(user, "id")
+
+    return getRecipeById(id)
+}
+
+function updateRecipe(id, recipe) {
+    return db("recipes")
+    .where("id", Number (id))
+    .update(recipe)
+}
+
+function deleteRecipe(id) {
+    return db("recipes")
+    .where("id", Number (id))
+    .del()
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser =require("cookie-parser")
 const authRouter = require("../auth-routers/authRouter")
+const recipesRouter = require("../recipes-router/recipesRouter")
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.use(express.json());
 server.use(cookieParser())
 
 server.use("/api/auth", authRouter)
+server.use("/api/recipes", recipesRouter)
 
 server.use("/", (req, res) => {
     res.json("API is running, welcome!!")
