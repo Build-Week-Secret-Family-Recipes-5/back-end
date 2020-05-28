ef00e3e76b5ee7eaeb88325a9bb56d835d5b26e6
unknown <fnumilat@gmail.com>
2020-05-27 22:32:17 -0500

    some more changes

--- migrations/20200521202315_recipes-table.js
+++ migrations/20200521202315_recipes-table.js
@@ -3,11 +3,11 @@ exports.up = async function(knex) {
   await knex.schema.createTable("recipes", table => {
       table.increments("id")
       table.string("title").notNull()
-      table.string("ingredients").notNull()
-      table.string("instructions").notNull()
+      table.string("ingredients", 500).notNull()
+      table.string("instructions", 500).notNull()
       table.string("source").notNull()
       table.string("category").notNull()
-      table.string("photo").notNull
+      table.string("photo", 500).notNull
   })
 };
 
